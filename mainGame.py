4f150948f24492cad6c4c1f9acbd2126aa7b0920
Viktor-JN <viktor.johanssonnygren@elev.ntig.se>
2024-03-19 08:46:27 +0100

    skrev kommentarer på koden

--- mainGame.py
+++ mainGame.py
@@ -15,9 +15,9 @@ rock = 0 #}
 paper = 1 #} anger vilka nummer som sten, sax och påse har under spelet så det går att jämföra datorn och användarens svar
 scissor = 2 #}
 
-print(start.splash)
+print(start.splash) #Visar startskärmen med de olika knapparna man kan trycka på och grafiken för RPS
 while True:
-    ai = randint(0, 2)
+    ai = randint(0, 2) #Slumpar fram vad datorn väljer mellan sten, sax och påse
     user = getwch().upper() #Använder upper för att slippa använda stor och liten bokstav i if-satserna
     rounds +=1
     if user == "R":
@@ -25,12 +25,12 @@ while True:
         if ai == rock:
             print(f"You chose:\n{hands.rock}\n\nAi chose:\n{hands.rock}\n{bcolors.YELLOW}It's a tie!{bcolors.DEFAULT}") #Visar vad du och ai valde och visar att det blev lika
             ties += 1
-            print(f"Wins: {wins} Ties: {ties} Losses: {losses} Rounds: {rounds}")
+            print(f"Wins: {wins} Ties: {ties} Losses: {losses} Rounds: {rounds}")# skriver ut hur många rundor, lika, vinster och förluster spelaren har
             print("""To continue playing, press either (R)ock, (P)aper or (S)cissors
 Press Q to quit""")
 
         elif ai == paper:
-            print(f"You chose:\n{hands.rock}\n\nAi chose:\n{hands.paper}\n{bcolors.RED}You lost!{bcolors.DEFAULT}")
+            print(f"You chose:\n{hands.rock}\n\nAi chose:\n{hands.paper}\n{bcolors.RED}You lost!{bcolors.DEFAULT}") #Återställer färgen till default på slutet så texten efter inte får en annan färg
             losses +=1
             print(f"Wins: {wins} Ties: {ties} Losses: {losses} Rounds: {rounds}")
             print("""To continue playing, press either (R)ock, (P)aper or (S)cissors
